753a7416dd8585ec7e68b8c76480e569b101cca1
alemansour957-netizen <alemansour957@gmail.com>
2026-04-14 21:26:31 +0300

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,5 @@
+import { BaseService } from './src/services/index.js';
+import fs from 'fs';
+
+// To run this ts script, I will just use node if compiled, or ts-node.
+// better yet, just write a plain JS script reading the DB.
